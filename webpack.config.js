09e57b34ba8a79832c5b5f5bf8012569ea2e6496
plantarflex <plantarflex@naver.com>
2021-04-19 04:41:14 +0900

    add webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const TerserPlugin = require('terser-webpack-plugin')
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+	output: 
+  {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: {
+      name: 'cornerstoneTools-SyncBrushTool',
+      type: 'umd',
+      umdNamedDefine: true,
+    },
+    clean: true,
+  },
+  optimization: {
+    minimizer: [new TerserPlugin({
+      extractComments: false,
+    })],
+  },
+};
